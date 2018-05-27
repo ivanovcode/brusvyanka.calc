a17b5f3fd75618326c33ceecea657f218c9fb6c5
Vladimir V. Ivanov <development@ivanov.site>
2018-05-27 20:18:48 +0300

    debug first release

--- index.php
+++ index.php
@@ -9,9 +9,17 @@
 
 	if(empty($alias)) die("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
-	$db = new SQLite3(realpath('db.db'));
+
+	if (file_exists(realpath('db.db'))) {
+	    echo "Файл $filename существует";
+	} else {
+	    echo "Файл $filename не существует";
+	}
 	echo "test";
 	die();
+
+	$db = new SQLite3(realpath('db.db'));
+
 	$results = $db->query("SELECT products.article, products.area, products.rooms, products.floors FROM products WHERE products.alias = '".$alias."'");
 	$rows = $results->fetchArray();
 	
