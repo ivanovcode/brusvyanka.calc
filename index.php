473d2b0b4081f8344d8f8c259f23b576a30f33e6
Vladimir V. Ivanov <development@ivanov.site>
2018-05-27 20:05:24 +0300

    debug first release

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 	$httpReferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
 	$httpReferer = "http://brusvyanka.ru/proekty-domov/tayga/";	
-	echo $_GET['referer'];
-	die();
+
 	$httpReferer = $_GET['referer'];
 
 	$alias = end(array_filter(explode("/", parse_url($httpReferer, PHP_URL_PATH))));	
-
+	echo $alias;
+	die();	
 	if(empty($alias)) die("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
 	$db = new SQLite3('db.db');
